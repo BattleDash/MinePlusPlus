97b87ee8177b9af24b9e9a82906cdd7f48c7494c
BattleDash <itsbattledash@gmail.com>
2022-01-03 02:08:18 -0700

    Plugin system

---
+++ Source/Private/Server/DedicatedServer.cpp
@@ -0,0 +1,43 @@
+// Copyright BattleDash. All Rights Reserved.
+
+#include <Base/Log.h>
+#include <Base/Version.h>
+#include <Core/ServerProperties.h>
+#include <Server/DedicatedServer.h>
+
+#include <chrono>
+#include <thread>
+
+namespace mpp
+{
+DedicatedServer::DedicatedServer(const ServerProperties* properties)
+: MinecraftServer(properties)
+{
+}
+
+DedicatedServer::~DedicatedServer()
+{
+}
+
+void DedicatedServer::InitializeServer()
+{
+    // TODO: Server console handler thread
+
+    MPP_LOG(LogLevel::Info, "MinePlusPlus server starting on port " << m_properties->serverPort << "...");
+    MPP_LOG(LogLevel::Info, "Default game type: " << m_properties->gamemode->m_name);
+    MPP_LOG(LogLevel::Info,
+            "This server is running MinePlusPlus version " << MPP_VERSION << " (MC: " << MPP_MINECRAFT_VERSION << ")");
+
+    m_pluginLoader->LoadPlugins();
+    EnablePlugins();
+
+    MPP_LOG(LogLevel::Info, "Loaded and enabled plugins");
+
+
+
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+} // namespace mpp
